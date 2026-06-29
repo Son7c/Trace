307f8ad844de4b29fb9ea7e36418a0d8abbb77c7
Souvik Majee <souvikmajee039@gmail.com>
2026-06-29 23:01:06 +0530

    feat(notes): completed Notes CRUD API

--- app/api/problems/[id]/note/route.ts
+++ app/api/problems/[id]/note/route.ts
@@ -1,6 +1,6 @@
 import { auth } from "@/lib/auth";
 import prisma from "@/lib/prisma";
-import { CreateNoteSchema} from "@/lib/validators/notes";
+import { CreateNoteSchema, UpdateNoteSchema } from "@/lib/validators/notes";
 import { headers } from "next/headers";
 
 export async function POST(
@@ -78,5 +78,187 @@ export async function POST(
   });
   return Response.json(note, {
     status: 201,
+  });
+}
+
+export async function GET(
+  _request: Request,
+  { params }: RouteContext<"/api/problems/[id]/note">,
+) {
+  const { id } = await params;
+
+  const h = await headers();
+  const session = await auth.api.getSession({
+    headers: h,
+  });
+
+  if (!session) {
+    return Response.json(
+      { message: "Unauthorized" },
+      {
+        status: 401,
+      },
+    );
+  }
+  const problem = await prisma.problem.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      note: true,
+    },
+  });
+  if (!problem)
+    return Response.json({ message: "Problem Not Found" }, { status: 404 });
+
+  const userId = session.session.userId;
+  if (userId !== problem.userId) {
+    return Response.json(
+      {
+        message: "Forbidden",
+      },
+      { status: 403 },
+    );
+  }
+  if (!problem.note) {
+    return Response.json({ message: "Note not found" }, { status: 404 });
+  }
+  return Response.json(problem.note, {
+    status: 200,
+  });
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: RouteContext<"/api/problems/[id]/note">,
+) {
+  const req = await request.json();
+  const { id } = await params;
+  const parsed = UpdateNoteSchema.safeParse(req);
+  if (!parsed.success) {
+    return Response.json(
+      { error: parsed.error.flatten().fieldErrors },
+      { status: 400 },
+    );
+  }
+  const noteData = parsed.data;
+
+  const h = await headers();
+  const session = await auth.api.getSession({
+    headers: h,
+  });
+  if (!session) {
+    return Response.json(
+      { message: "Unauthorized" },
+      {
+        status: 401,
+      },
+    );
+  }
+
+  const userId = session.session.userId;
+  const problem = await prisma.problem.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      note: true,
+    },
+  });
+
+  if (!problem)
+    return Response.json({ message: "Problem Not Found" }, { status: 404 });
+
+  if (userId !== problem.userId) {
+    return Response.json(
+      {
+        message: "Forbidden",
+      },
+      { status: 403 },
+    );
+  }
+
+  if (!problem.note) {
+    return Response.json({ message: "Note not found" }, { status: 404 });
+  }
+
+  const updatedNote = await prisma.note.update({
+    where: {
+      id: problem.note.id,
+    },
+    data: {
+      ...noteData,
+    },
+  });
+
+  return Response.json(updatedNote, {
+    status: 200,
+  });
+}
+
+export async function DELETE(
+  _request: Request,
+  { params }: RouteContext<"/api/problems/[id]/note">,
+) {
+  const { id } = await params;
+
+  const h = await headers();
+  const session = await auth.api.getSession({
+    headers: h,
+  });
+  if (!session) {
+    return Response.json(
+      { message: "Unauthorized" },
+      {
+        status: 401,
+      },
+    );
+  }
+  const problem = await prisma.problem.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      note: true,
+    },
+  });
+
+  if (!problem) {
+    return Response.json(
+      { message: "Problem not found" },
+      {
+        status: 404,
+      },
+    );
+  }
+  const userId = session.session.userId;
+  if (userId !== problem.userId) {
+    return Response.json(
+      {
+        message: "Forbidden",
+      },
+      {
+        status: 403,
+      },
+    );
+  }
+
+  if (!problem.note) {
+    return Response.json(
+      { message: "Note not found" },
+      {
+        status: 404,
+      },
+    );
+  }
+
+  await prisma.note.delete({
+    where: {
+      id: problem.note.id,
+    },
+  });
+
+  return new Response(null, {
+    status: 204,
   });
 }
