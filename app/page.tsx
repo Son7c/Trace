4e2099fbee7c9571b6b55cf0d92d5d00f5d1cae0
Souvik Majee <souvikmajee039@gmail.com>
2026-07-20 00:53:54 +0530

    Added NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,21 @@ import { authClient } from "@/lib/auth-client";
 import { redirect } from "next/navigation";
 import { useState } from "react";
 import AuthForm from "@/components/AuthForm";
+import NavLanding from "@/components/navLanding";
 
 
 export default function Home() {
-  const { data: session, error, isPending } = authClient.useSession();
+  // const { data: session, error, isPending } = authClient.useSession();
   
-  if (isPending) return <div>Loading session...</div>;
+  // if (isPending) return <div>Loading session...</div>;
 
-  if (session) {
-    redirect("/dashboard");
-  }
+  // if (session) {
+  //   redirect("/dashboard");
+  // }
 
   return (
-    <div>
-      <AuthForm/>
+    <div className="bg-[#0A0A0B] min-h-screen w-full">
+      <NavLanding />
     </div>
   );
 }
---
+++ components/navLanding.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+
+export default function NavLanding() {
+  return (
+    <nav className="flex items-center justify-between px-12 py-3">
+      <div className="text-white text-3xl font-bold tracking-tight">Trace</div>
+      <div>
+        <a href="#product" className="text-white text-md">
+          Product
+        </a>
+        <a href="#how-it-works" className="text-white text-md ml-6">
+          How it works
+        </a>
+        <a href="#features" className="text-white text-md ml-6">
+          Features
+        </a>
+        <a href="#pricing" className="text-white text-md ml-6">
+          Pricing
+        </a>
+        <a href="#about" className="text-white text-md ml-6">
+          About
+        </a>
+      </div>
+
+      <div className="text-white text-md">
+        <button className="text-white text-md">Log in</button>
+        <button className="text-black text-md ml-5 bg-[#82c379] p-2 rounded-lg">
+          Start Training
+        </button>
+      </div>
+    </nav>
+  );
+}
